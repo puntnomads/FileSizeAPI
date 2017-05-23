b3f0570e24d3d55c625d3b550acec851e1fff2e6
Zakariya Moallin <puntnomads@gmail.com>
2017-05-23 16:50:58 +0000

    finished the functionality

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var app = express();
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var	path = require('path');
+
+app.use(bodyParser.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
-app.listen(8080, function () {
+app.post('/get-file-size', multer({ dest: './uploads/'}).single('upl'), function(req,res){
+    if(req.file != undefined){
+        var object = { size: req.file.size };
+        res.send(object);
+    } else {
+        res.send("Please upload a file before submitting. Thank you.");
+    }
+});
+
+app.listen(process.env.PORT || 8080, function () {
   console.log('Example app listening on port 8080!');
 });
